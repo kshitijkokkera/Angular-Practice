cf5309d48c33e3bf1f52110c06abb3c377859a5e
kshitijkokkera <kshitijkokkera@gmail.com>
2025-10-08 18:53:03 -0400

    feat: implement city filter functionality in Home component

--- first-app_01-hello-world/src/app/home/home.ts
+++ first-app_01-hello-world/src/app/home/home.ts
@@ -8,12 +8,12 @@ import {Housing} from '../housing';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
     <section class="results">
-      @for(housingLocation of housingLocationList; track $index) {
+      @for(housingLocation of filteredLocationList; track $index) {
         <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
       }
     </section>
@@ -23,7 +23,18 @@ import {Housing} from '../housing';
 export class Home {
   housingLocationList: HousingLocationInfo[] = [];
   housingService: Housing = inject(Housing);
+  filteredLocationList: HousingLocationInfo[] = [];
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
   }
 }
